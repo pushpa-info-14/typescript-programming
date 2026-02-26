f75ba030b90a12d4bc81304740c772aaa6e870a2
pushpa.info.14 <pushpa.info.14@gmail.com>
2026-02-26 14:18:23 +0530

    LeetCode 1404

--- src/LeetCode/1250-1500/Q1404 Number of Steps to Reduce a Number in Binary Representation to One.ts
+++ src/LeetCode/1250-1500/Q1404 Number of Steps to Reduce a Number in Binary Representation to One.ts
@@ -12,9 +12,31 @@ function numSteps(s: string): number {
 	return res;
 }
 
+function numSteps2(s: string): number {
+	let res = 0;
+	let carry = 0;
+	for (let i = s.length - 1; i > 0; i--) {
+		let bit = parseInt(s[i]!) + carry;
+		if (bit % 2 == 1) {
+			res += 2;
+			carry = 1;
+		} else {
+			res += 1;
+		}
+	}
+	return res + carry;
+}
+
 console.log(numSteps("1101"));
 console.log(numSteps("10"));
 console.log(numSteps("1"));
 console.log(
 	numSteps("1111011110000011100000110001011011110010111001010111110001"),
+);
+console.log("----------------------------");
+console.log(numSteps2("1101"));
+console.log(numSteps2("10"));
+console.log(numSteps2("1"));
+console.log(
+	numSteps2("1111011110000011100000110001011011110010111001010111110001"),
 );
